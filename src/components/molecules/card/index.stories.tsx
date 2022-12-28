92d32f72f24643b1401e3b4f6c305ac863dff869
Francisco Jimenez Aguilera <franjimenezagilera@gmail.com>
2022-12-28 00:06:14 +0000

    Add dummy Card component

---
+++ src/components/molecules/card/index.stories.tsx
@@ -0,0 +1,18 @@
+import React, { Children } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Card from "./index";
+
+export default {
+  title: "Components/Molecules/Card",
+  component: Card,
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  id: 1,
+  title: "bulbasaur",
+  imageUrl:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+};
---
+++ src/components/molecules/card/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export interface CardProps {
+  id: number;
+  title: string;
+  imageUrl: string;
+}
+
+const Card = ({ id, title, imageUrl }: CardProps) => {
+  return (
+    <div className={styles.root}>
+      <h1>{id}</h1>
+      <h2>{title}</h2>
+      <img src={imageUrl} />
+    </div>
+  );
+};
+
+export default Card;
